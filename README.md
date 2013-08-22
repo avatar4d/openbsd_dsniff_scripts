dc3ad5a7aa2dc524eb8c2b46606587e863ba4911
Chad Gross <avatar4d@gmail.com>
2013-08-22 17:20:33 -0400

    Updating LICENSE and README files and adding script to the project

--- README.md
+++ README.md
@@ -1,4 +1,34 @@
 openbsd_dsniff_scripts
 ======================
+OpenBSD dsniff agent rc scripts
 
 This is a script that will startup, shutdown, and restart dsniff, mailsnarf, msgsnarf and urlsnarf listening agents on OpenBSD routers/firewalls. It also logs their output and rotates the files  when they reach 50MB.
+
+By default it logs to /var/log/dsniff/$APPNAME
+
+
+Usage:
+
+        sudo su -
+        mkdir ~/bin
+        cd ~/bin
+        git clone git://github.com/avatar4d/openbsd_dsniff_scripts.git
+        crontab -e
+
+
+Then add the following:
+
+        0       0       *       *       *       /root/bin/openbsd_dsniff_scripts/sniffer_agents.sh
+
+Then edit the rc.local script:
+
+        vi /etc/rc.local
+
+Then add the following:
+
+       ## start dsniff daemons
+       if [ -x /root/bin/sniff.sh ]; then
+            echo 'dsniff daemons'
+            /root/bin/sniff.sh
+       fi
+
---
+++ sniffer_agents.sh
@@ -0,0 +1,107 @@
+#!/bin/ksh
+
+#VARIABLES
+  CMD=$@
+##Logs
+  LOGDIR=/var/log/dsniff
+  DSNIFF_LOG=dsniff
+  MAILSNARF_LOG=mailsnarf
+  MSGSNARF_LOG=msgsnarf
+  URLSNARF_LOG=urlsnarf
+##App Flags
+##CHANGE THESE TO WHATEVER IS APPROPRIATE FOR YOU
+  DSNIFF_FLAGS="not host youtube.com and not host supertorrents.org and not host iptorrents.com and not host 172.16.50.10"
+  URLSNARF_FLAGS="not host supertorrents.org and not host iptorrents.com and not host 172.16.50.10"
+  INTERFACE=vr0 
+
+
+#FUNCTIONS
+die(){
+#
+#Kills each app if it is running
+#
+  if [ -n `pgrep dsniff` ];
+  then
+    pkill dsniff
+  fi
+
+  if [ -n `pgrep mailsnarf` ];
+  then
+    pkill mailsnarf
+  fi
+
+  if [ -n `pgrep urlsnarf` ];
+  then
+    pkill urlsnarf
+  fi
+
+  if [ -n `pgrep msgsnarf` ];
+  then
+    pkill msgsnarf
+  fi
+}
+
+logrotate(){
+#
+#This checks for each application's log, checks if it is 50MB or more.
+#It then proceeds to rotate the compressed archives (it keeps 5) and
+#compresses the current log after renaming it.
+#
+for LOG in [[ $DSNIFF_LOG $MAILSNARF_LOG $MSGSNARF_LOG $URLSNARF_LOG ]]; 
+do
+  if [[ -e $LOGDIR/$LOG && `ls -la $LOGDIR/$LOG | awk '{print $5}'` -ge 52428800 ]];
+  then
+    if [ -e $LOGDIR/$LOG.4.gz ];
+    then
+      mv $LOGDIR/$LOG.4.gz $LOGDIR/$LOG.5.gz
+    fi
+
+    if [[ -e $LOGDIR/$LOG.3.gz ]];
+    then
+      mv $LOGDIR/$LOG.3.gz $LOGDIR/$LOG.4.gz
+    fi
+
+    if [[ -e $LOGDIR/$LOG.2.gz ]];
+    then
+      mv $LOGDIR/$LOG.2.gz $LOGDIR/$LOG.3.gz
+    fi
+
+    if [[ -e $LOGDIR/$LOG.1.gz ]];
+    then
+      mv $LOGDIR/$LOG.1.gz $LOGDIR/$LOG.2.gz
+    fi
+
+    mv $LOGDIR/$LOG $LOGDIR/$LOG.1
+    gzip -o $LOGDIR/$LOG.1.gz $LOGDIR/$LOG.1 & 
+
+  fi
+done
+}
+
+start(){
+#
+#Starts each application with any necessary flags (configured in the VARIABLES section above)
+#
+/usr/local/sbin/dsniff -i $INTERFACE -m -w $LOGDIR/$DSNIFF_LOG $DSNIFF_FLAGS &
+/usr/local/sbin/mailsnarf -i $INTERFACE >> $LOGDIR/$MAILSNARF_LOG &
+/usr/local/sbin/msgsnarf -i $INTERFACE >> $LOGDIR/$MSGSNARF_LOG &
+/usr/local/sbin/urlsnarf -i $INTERFACE >> $LOGDIR/$URLSNARF_LOG $URLSNARF_FLAGS &
+}
+
+#EXECUTION
+case $CMD in 
+start)
+  logrotate
+  start
+  ;;
+
+stop)
+  die
+  ;;
+
+restart)
+  shutdown
+  logrotate
+  start
+  ;;
+esac
